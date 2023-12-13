df96a2576095ff77b28ec99f3ee2a543a9c6d026
JevMaks <123833233+JevMak@users.noreply.github.com>
2023-12-13 22:20:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const app = express();
 const db = require('./db');
 const userRouter = require('./routes/users.routes');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
+
 app.use(express.json());
 app.use(bodyParser.json());
 
